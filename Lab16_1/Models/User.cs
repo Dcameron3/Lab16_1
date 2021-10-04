80f3f253c3ab407aa81b568b78fcd0552e3054c1
Devin Cameron <devincameron@quickenloans.com>
2021-10-04 19:56:20 -0400

    first commit

---
+++ Lab16_1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16_1.Models
+{
+    public class User
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
